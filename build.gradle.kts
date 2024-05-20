98d9b82b824f8170ba4e6d928aa0e41518cc1d50
vAHiD <Vahid.GhaziNour@gmail.com>
2024-05-20 23:46:09 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,3 +70,10 @@ subprojects {
 			maven { url 'https://jitpack.io' }
 		}
 	}
+dependencyResolutionManagement {
+		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+		repositories {
+			mavenCentral()
+			maven { url 'https://jitpack.io' }
+		}
+	}
